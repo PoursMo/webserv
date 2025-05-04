07b9c1bb784a3e5012b648efb81350fb109cc6cf
Amaury <loubry.amaury@gmail.com>
2025-05-04 21:34:19 +0200

    working on poll class

--- srcs/Poll.cpp
+++ srcs/Poll.cpp
@@ -3,6 +3,7 @@
 #include <unistd.h>
 #include <sys/epoll.h>
 
+#include <vector>
 #include <stdexcept>
 #include <string.h>
 #include <errno.h>
@@ -16,43 +17,49 @@ edge-trigerred mode: Continue to receive events until the underlying file descri
 class Poll
 {
 private:
-	int fd;
+	int pollFd;
+	std::vector<struct epoll_event> events;
 
 public:
 	Poll()
 	{
-		int fd = epoll_create(1); // adjust size ?
-		if (fd == -1)
+		int pollFd = epoll_create(1); // adjust size ?
+		if (pollFd == -1)
 			throw std::runtime_error(strerror(errno));
 	}
 	~Poll()
 	{
-		close(fd);
+		close(pollFd);
 	}
-	void add(int new_fd)
+
+	void add(int fd)
 	{
 		struct epoll_event event;
 		event.events = EPOLLIN;
-		event.data.fd = new_fd;
-		if (epoll_ctl(fd, EPOLL_CTL_ADD, new_fd, &event) == -1)
-			throw std::runtime_error(strerror(errno));
+		event.data.fd = fd;
+		if (epoll_ctl(pollFd, EPOLL_CTL_ADD, fd, &event) == -1)
+			throw std::runtime_error(strerror(errno)); // other error ?
 	}
-	// add fd
-	// remove fd
+	void del(int fd)
+	{
+		if (epoll_ctl(pollFd, EPOLL_CTL_DEL, fd, 0) == -1)
+			throw std::runtime_error(strerror(errno)); // other error ?
+	}
+	int wait()
+	{
+		int nready = epoll_wait(pollFd, events.data(), std::min(events.size(), (size_t)10), -1); // change 10 ?
+		if (nready == -1)
+			throw std::runtime_error(strerror(errno)); // other error ?
+		return nready;
+	}
 };
 
 void poll_loop()
 {
-
-	struct epoll_event events[10]; // adjust 10 value
-
+	// create poll
 	while (1)
 	{
-		int nready = epoll_wait(epoll_fd, events, 10, -1); // adjust 10 value
-		if (nready < -1)
-		{
-			throw std::runtime_error(strerror(errno));
-		}
+		// wait
 
 		for (int i = 0; i < nready; i++)
 		{
@@ -77,10 +84,7 @@ void poll_loop()
 				if (recv(fd, buffer, clientMaxBodySize, 0) == 0)
 				{
 					std::cout << "Client with fd: " << fd << " disconnected" << std::endl;
-					if (epoll_ctl(epoll_fd, EPOLL_CTL_DEL, fd, 0) == -1)
-					{
-						throw std::runtime_error(strerror(errno));
-					}
+					// remove
 					close(fd);
 				}
 				else
@@ -96,8 +100,4 @@ void poll_loop()
 			}
 		}
 	}
-
-	close(epoll_fd);
-
-	std::cout << "Server initialized and listening on " << address << ":" << port << std::endl;
 }
