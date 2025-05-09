4630c70436725399cc47cd9c4eb7585d468ce854
Loïc Baecher <74916339+loicbaecher@users.noreply.github.com>
2025-05-09 14:43:59 +0200

    add parsing of first line

--- srcs/Request.cpp
+++ srcs/Request.cpp
@@ -1,4 +1,5 @@
 #include "Request.hpp"
+#include <stdexcept>
 
 Request::Request(/* args */)
 {
@@ -8,31 +9,96 @@ Request::~Request()
 {
 }
 
-bool isMethod(char *lstart, char *lend, std::string method)
+bool isSupportedMethod(char **lstart, char *lend, std::string method)
 {
+	int	i = 0;
 
+	while (**lstart != ' ' && *lstart != lend)
+	{
+		if (**lstart != method[i])
+			return false,
+		(*lstart)++;
+	}
+	return true;
+}
+
+Method setMethod(char *lstart, char *lend)
+{
+	int	i = 0;
+	std::string method;
+
+	std::vector<std::string> existing_methods = {"GET", "HEAD", "POST", "PUT", "DELETE", "CONNECT", "OPTIONS", "TRACE"};
+	for (std::vector<std::string>::iterator s = existing_methods.begin(); s != existing_methods.end(); s++)
+	{
+		method = *s;
+		int	i = 0;
+		while (*lstart != ' ' && lstart != lend && i < method.size())
+		{
+			if (*lstart != method[i])
+				method = "";
+			lstart++;
+			i++;
+		}
+		if (method != "")
+			break ;
+	}
+	if (method == "")
+		throw http_error(400);
+	if (method == "GET")
+		return GET;
+	else if (method == "POST")
+		return POST;
+	else if (method == "DELETE")
+		return DELETE;
+	else
+		throw http_error(501);;
+	return GET;
+}
+
+std::string Request::setResource(char **lstart, char *lend)
+{
+	std::string resource;
+
+	while (**lstart != *lend && *lstart != lend)
+	{
+		resource = resource + **lstart;
+		(*lstart)++;
+	}
+	if (resource == "")
+		throw http_error(400);
+	return (resource);
+}
+
+bool isValidProtocol(char *lstart, char *lend)
+{
+	int	i = 0;
+	std::string protocol = "HTTP/1.1";
+	while (lstart != lend)
+	{
+		if (*lstart != protocol[i])
+			return false;
+		i++;
+		lstart++;
+	}
+	return true;
 }
 
 void Request::parseFirstLine(char *lstart, char *lend)
 {
-	if (*lstart == 'G')
-	{
-		if (!isMethod(lstart, lend, "GET"))
-			//ERROR
-	}
-	else if (*lstart == 'P')
-	{
-		if (!isMethod(lstart, lend, "POST"))
-			//ERROR
-	}
-	else if (*lstart == 'D')
-	{
-		if (!isMethod(lstart, lend, "DELETE"))
-			//ERROR
-	}
-	else
-		//Throw Error 501, unsupported method or 401 error
-	}
+	this->method = setMethod(lstart, lend);
+	while (lstart != lend && *lstart != ' ')
+		lstart++;
+	if (*lstart != ' ' || lstart == lend)
+		throw http_error(400);
+	lstart++;
+	this->resource = setResource(&lstart, lend);
+	if (*lstart != ' ' || lstart == lend)
+		throw http_error(400);
+	lstart++;
+	if(!isValidProtocol(lstart, lend))
+		throw http_error(400);
+	if (lstart != lend)
+		throw http_error(400);
 }
 
 void Request::parseHeaderLine(char *lstart, char *lend)
@@ -48,9 +114,20 @@ void Request::parseRequestLine(char *lstart, char *lend)
 		parseHeaderLine(lstart, lend);
 }
 
+const char *Request::UnrecognizedMethod::what() const throw()
+{
+	return ("Method not recognized");
+}
+
+const char *Request::UnsupportedMethod::what() const throw()
+{
+	return ("Method not supported");
+}
+
 int main()
 {
 
 
 	return 0;
 }
+
--- srcs/Request.hpp
+++ srcs/Request.hpp
@@ -4,25 +4,37 @@
 #include <map>
 #include <string>
 #include "LocationData.hpp"
+#include "http_error.hpp"
 
 class Request
 {
-private:
-    Method method;
-    std::string resource;
-    std::map<std::string, std::string> headers;
-    int bodyFd = -1;
-	void parseFirstLine(char *lstart, char *lend);
-	void parseHeaderLine(char *lstart, char *lend);
-    void addHeader();
+	private:
+		Method method;
+		std::string resource;
+		std::map<std::string, std::string> headers;
+		int bodyFd = -1;
+		void parseFirstLine(char *lstart, char *lend);
+		Method setMethod(char *lstart, char *lend);
+		std::string setResource(char **lstart, char *lend);
+		void parseHeaderLine(char *lstart, char *lend);
+		void addHeader();
 
-public:
+	public:
+		Request();
+		~Request();
+		void parseRequestLine(char *lstart, char *lend);
+		void setBodyFd();
+		void execute();
 
-    Request();
-    ~Request();
-    void parseRequestLine(char *lstart, char *lend);
-    void setBodyFd();
-    void execute();
+	class UnrecognizedMethod: public std::exception
+	{
+		const char *what() const throw();
+	};
+
+	class UnsupportedMethod: public std::exception
+	{
+		const char *what() const throw();
+	};
 };
 
 #endif
