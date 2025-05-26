64369bff39a402e6546b08e8b83ace44a2df5f22
Loïc Baecher <74916339+loicbaecher@users.noreply.github.com>
2025-05-26 14:41:25 +0200

    php explore

---
+++ www/php/delete.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>File_deletion</title>
+</head>
+<body>
+	<div>
+	<?php
+	if ($_SERVER['REQUEST_METHOD'] === 'DELETE') {
+		$resource = isset($_GET['resource']) ? $_GET['resource'] : null;
+		$baseDir = realpath(__DIR__ . '/files_to_delete');
+		$targetPath = realpath($baseDir . '/' . $resource);
+		echo $resource;
+		echo $baseDir;
+		echo $targetPath;
+		if (file_exists($targetPath)) {
+			if (unlink($targetPath)) {
+				echo "deletion succeeded";
+			}
+		}
+		else
+		{
+			echo "File does not exist";
+		}
+	}
+	?>
+	</div>
+</body>
+</html>
